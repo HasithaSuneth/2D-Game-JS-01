475271f792a8c1f4c81b3afd3a7fc59de65f4643
Hasitha Suneth <87106402+HasithaSuneth@users.noreply.github.com>
2022-08-15 15:20:25 +0530

    Update main.js

--- 2DGameJS/main.js
+++ 2DGameJS/main.js
@@ -1,6 +1,7 @@
 import { Background } from "./background.js";
 import { ClimbingEnemy, FlyingEnemy, GroundEnemy } from "./enemies.js";
 import InputHandler from "./input.js";
+import { Mobile } from "./mobile.js";
 import { Player } from "./player.js";
 import { Sound } from "./sounds.js";
 import { UI } from "./ui.js";
@@ -12,7 +13,6 @@ window.addEventListener("load", function () {
   const ctx = canvas.getContext("2d");
   canvas.width = 1000;
   canvas.height = 500;
-
   class Game {
     constructor(width, height) {
       this.width = width;
@@ -20,11 +20,16 @@ window.addEventListener("load", function () {
       this.groundMargin = 40; // City: 80 Wild: 40
       this.speed = 0;
       this.maxSpeed = 5;
+      this.isMobileDevice = window.matchMedia(
+        "only screen and (max-width: 1000px)"
+      ).matches;
+      this.boundingBox = canvas.getBoundingClientRect();
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler(this);
       this.UI = new UI(this);
       this.sound = new Sound();
+      this.mobile = new Mobile(this);
       this.enemies = [];
       this.particles = [];
       this.collisions = [];
@@ -48,6 +53,7 @@ window.addEventListener("load", function () {
       this.sound.startSound();
     }
     update(deltaTime) {
+      // console.log(this.input.keys);
       // Game over check
       this.time += deltaTime;
       if (this.time > this.maxTime) this.gameOver = true;
@@ -100,6 +106,10 @@ window.addEventListener("load", function () {
     draw(context) {
       this.background.draw(context);
       this.player.draw(context);
+      if (this.isMobileDevice) {
+        this.mobile.restartLogo(context);
+        this.mobile.powerButton(context);
+      }
       this.enemies.forEach((enemy) => {
         enemy.draw(context);
       });
