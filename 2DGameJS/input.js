5ffff7df49f464d1fe5b8100f9175019e84246af
Hasitha Suneth <87106402+HasithaSuneth@users.noreply.github.com>
2022-08-15 18:53:08 +0530

    Update input.js

--- 2DGameJS/input.js
+++ 2DGameJS/input.js
@@ -31,6 +31,7 @@ export default class InputHandler {
       }
       // console.log(e.key, this.keys);
     });
+
     window.addEventListener("touchstart", (e) => {
       this.touchY = e.changedTouches[0].pageY;
       this.touchX = e.changedTouches[0].pageX;
@@ -60,7 +61,92 @@ export default class InputHandler {
       ) {
         document.location.reload(true);
       }
+      // Touch Press
+      // Right
+      if (
+        e.changedTouches[0].pageX >=
+          this.game.boundingBox.x +
+            this.game.boundingBox.width -
+            this.game.boundingBox.width / 5.8 &&
+        e.changedTouches[0].pageX <=
+          this.game.boundingBox.x +
+            this.game.boundingBox.width -
+            this.game.boundingBox.width / 10.5 &&
+        e.changedTouches[0].pageY >=
+          this.game.boundingBox.y +
+            this.game.boundingBox.height -
+            this.game.boundingBox.height / 5.4 &&
+        e.changedTouches[0].pageY <=
+          this.game.boundingBox.y + this.game.boundingBox.height &&
+        this.keys.indexOf("ArrowRight") === -1
+      ) {
+        this.keys.push("ArrowRight");
+      }
+      // Down
+      if (
+        e.changedTouches[0].pageX >=
+          this.game.boundingBox.x +
+            this.game.boundingBox.width -
+            this.game.boundingBox.width / 3.7 &&
+        e.changedTouches[0].pageX <=
+          this.game.boundingBox.x +
+            this.game.boundingBox.width -
+            this.game.boundingBox.width / 5.2 &&
+        e.changedTouches[0].pageY >=
+          this.game.boundingBox.y +
+            this.game.boundingBox.height -
+            this.game.boundingBox.height / 5.4 &&
+        e.changedTouches[0].pageY <=
+          this.game.boundingBox.y + this.game.boundingBox.height &&
+        this.keys.indexOf("ArrowDown") === -1
+      ) {
+        this.keys.push("ArrowDown");
+      }
+      // Left
+      if (
+        e.changedTouches[0].pageX >=
+          this.game.boundingBox.x +
+            this.game.boundingBox.width -
+            this.game.boundingBox.width / 2.6 &&
+        e.changedTouches[0].pageX <=
+          this.game.boundingBox.x +
+            this.game.boundingBox.width -
+            this.game.boundingBox.width / 3.4 &&
+        e.changedTouches[0].pageY >=
+          this.game.boundingBox.y +
+            this.game.boundingBox.height -
+            this.game.boundingBox.height / 5.4 &&
+        e.changedTouches[0].pageY <=
+          this.game.boundingBox.y + this.game.boundingBox.height &&
+        this.keys.indexOf("ArrowLeft") === -1
+      ) {
+        this.keys.push("ArrowLeft");
+      }
+      // Up
+      if (
+        e.changedTouches[0].pageX >=
+          this.game.boundingBox.x +
+            this.game.boundingBox.width -
+            this.game.boundingBox.width / 3.7 &&
+        e.changedTouches[0].pageX <=
+          this.game.boundingBox.x +
+            this.game.boundingBox.width -
+            this.game.boundingBox.width / 5.2 &&
+        e.changedTouches[0].pageY >=
+          this.game.boundingBox.y +
+            this.game.boundingBox.height -
+            this.game.boundingBox.height / 2.5 &&
+        e.changedTouches[0].pageY <=
+          this.game.boundingBox.y +
+            this.game.boundingBox.height -
+            this.game.boundingBox.height / 4.3 &&
+        this.keys.indexOf("ArrowUp") === -1
+      ) {
+        this.keys.push("ArrowUp");
+      }
     });
+    // Touch Swipe Contols
+    /*
     window.addEventListener("touchmove", (e) => {
       const swipeDistanceY = e.changedTouches[0].pageY - this.touchY;
       const swipeDistanceX = e.changedTouches[0].pageX - this.touchX;
@@ -86,12 +172,13 @@ export default class InputHandler {
         this.keys.push("ArrowRight");
       }
     });
+    */
     window.addEventListener("touchend", (e) => {
       this.keys.splice(this.keys.indexOf("ArrowUp"), 1);
       this.keys.splice(this.keys.indexOf("ArrowDown"), 1);
       this.keys.splice(this.keys.indexOf("ArrowLeft"), 1);
       this.keys.splice(this.keys.indexOf("ArrowRight"), 1);
-      // this.keys.splice(this.keys.indexOf("Control"), 1);
+      this.keys.splice(this.keys.indexOf("Control"), 1);
     });
   }
 }
